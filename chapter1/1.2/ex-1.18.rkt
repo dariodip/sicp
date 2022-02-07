321b9c643e05a7e24ef1749e682b4adf47675978
dariodip <darionovantadue@gmail.com>
2022-02-07 18:07:01 +0100

    Exercises 1.14~1.23

--- chapter1/1.2/ex-1.18.rkt
+++ chapter1/1.2/ex-1.18.rkt
@@ -1,2 +1,19 @@
 ; Exercise 18
 #lang sicp
+
+; Using the results of exercises 1.16 and 1.17, devise a procedure that generates an iterative process
+; for multiplying two integers in terms of adding, doubling, and halving and uses a logarithmic number
+; of steps.
+
+(define (double n) (+ n n))
+(define (halve n) (/ n 2))
+
+; a * 2b = 2a * b
+
+(define (prod a b)
+  (fast-prod a b 0))
+
+(define (fast-prod a b acc)
+  (cond ((= b 0) acc)
+        ((even? b) (fast-prod a (halve b) (+ (double a) (double a))))
+        (else (fast-prod a (- b 1) (+ a a)))))
--- chapter1/1.2/ex-1.20.rkt
+++ chapter1/1.2/ex-1.20.rkt
@@ -1,2 +1,72 @@
 ; Exercise 20
 #lang sicp
+
+; The process that a procedure generates is of course dependent on the rules used by the interpreter.
+; As an example, consider the iterative gcd procedure given below.
+; Suppose we were to interpret this procedure using normal-order evaluation,
+; as discussed in section 1.1.5. (The normal-order-evaluation rule for if is described in exercise 1.5.).
+; Using the substitution method (for normal order), illustrate the process generated in evaluating (gcd 206 40)
+; and indicate the remainder operations that are actually performed.
+; How many remainder operations are actually performed in the normal-order evaluation of (gcd 206 40)?
+; In the applicative-order evaluation?
+;
+; Iterative procedure for calculating GCD of two numbers
+;
+; moved to common for reuse by other parts
+;
+; (define (gcd a b)
+;  (if (= b 0)
+;      a
+;      (gcd b (remainder a b))))
+;
+; Normal order evaluation:
+; (gcd 206 40) ; a = 206, b = 40
+
+; (if (= 40 0) 0)
+; (gcd 40 (remainder 206 40))
+
+; a = 40, b = (remainder 206 40)
+; (if (= (remainder 206 40) 0) a) -> (if (= 6 0) a) ; +1
+; (gcd (remainder 206 40)
+;      (remainder 40 (remainder (206 40))))
+
+; a = (remainder 206 40), b = (remainder 40 (remainder (206 40)))
+; (if (= (remainder 40 (reaminder 206 40)) 0) a) -> (if (= 4 0) a) ; +2 = 3
+; (gcd (remainder 40 (remainder (206 40)))
+;      (remainder (remainder 206 40) (remainder 40 (remainder (206 40)))))
+;
+; a = (remainder 40 (remainder (206 40))
+; b = (remainder (remainder 206 40) (remainder 40 (remainder (206 40))))
+; (if (= (remainder (remainder 206 40) (remainder 40 (remainder 206 40))) 0) a) ->
+; (if (= 2 0) a) -> ; +4 = 7
+; (gcd
+;  (remainder (remainder 206 40) (remainder 40 (remainder (206 40))))
+;  (remainder (remainder 40 (remainder (206 40)))
+;             (remainder (remainder 206 40) (remainder 40 (remainder (206 40))))))
+
+; a = (remainder (remainder 206 40) (remainder 40 (remainder (206 40))))
+; b = (remainder (remainder 40 (remainder (206 40))) (remainder (remainder 206 40) (remainder 40 (remainder (206 40)))))
+
+; (if (= (remainder (remainder 40 (remainder 206 40)) (remainder (remainder 206 40) (remainder 40 (remainder 206 40)))) 0) -> evaluating to 0
+;        (remainder
+;             (remainder 206 40)
+;             (remainder 40 (remainder 206 40)))) -> true +11 = 14
+
+
+; Applicative order of evaluation:
+; (gcd 206 40)
+; (if (= 40 0) 206)
+; (gcd 40 (remainder 206 40)) -> (gcd 40 6)
+;
+; (if (= 6 0) 40)
+; (gcd 6 (remainder 40 6)) -> (gcd 6 4)
+;
+; (if (= 4 0) 6)
+; (gcd 4 (remainder 6 4)) -> (gcd 4 2)
+;
+; (if (= 2 0) 4)
+; (gcd 2 (remainder 6 2)) -> (gcd 2 0)
+;
+;(if (= 0 0) 2) -> 2
+;
+; Total = 4
