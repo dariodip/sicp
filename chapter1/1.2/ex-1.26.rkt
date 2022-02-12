9fbe31c6e9a70441f964ea0ce1bbe4f29b13fb57
dariodip <darionovantadue@gmail.com>
2022-02-12 20:01:55 +0100

    Ex 1.26, 1.27

--- chapter1/1.2/ex-1.26.rkt
+++ chapter1/1.2/ex-1.26.rkt
@@ -20,3 +20,13 @@
 ; ``I don't see what difference that could make,'' says Louis.
 ; ``I do.'' says Eva. ``By writing the procedure like that, you have transformed the (log n) process
 ; into a (n) process.'' Explain.
+
+; If we know that applicative substitution evaluates parameters every time then
+; it is evaluating 2 times expmod for every expmod call. Therefore, despite the
+; fact we are halving the problem we are doubling the number of calls. So at the
+; end we are cancelling logarithm with exponend and then getting to the O(n)
+; order of growth.
+;
+; The one that do squaring instead of multiplication has only one call per
+; recursion and with halving the size of the problem every time it is converging
+; logarithmicaly.
--- chapter1/1.2/ex-1.27.rkt
+++ chapter1/1.2/ex-1.27.rkt
@@ -6,3 +6,37 @@
 ; That is, write a procedure that takes an integer n and tests whether an is
 ; congruent to a modulo n for every a < n,and try your procedure on the given
 ; Carmichael numbers.
+
+(define (pick-a-prime n)
+  (cond
+    ((= n 1) 561)
+    ((= n 2) 1105)
+    ((= n 3) 1729)
+    ((= n 4) 2465)
+    ((= n 5) 2821)
+    ((= n 6) 6601)
+    (else 1))
+  )
+
+(define (run-tests n)
+  (cond ((= (pick-a-prime n) 1)
+         (display "done")
+         (newline))
+        (else
+         (test-carmichael-number (pick-a-prime n))
+         (run-tests (- n 1)))))
+
+(define (test-carmichael-number n)
+  (carmichael-test-iteration 1 n true))
+
+(define (carmichael-test-iteration a n is-it?)
+  (cond ((= a n)
+         (display n)
+         (display ": ")
+         (display is-it?)
+         (newline))
+        (else
+         (carmichael-test-iteration (+ a 1) n (and is-it? (fermat-test a n))))))
+
+(define (fermat-test a n)
+  (= (expmod a n n) a))
